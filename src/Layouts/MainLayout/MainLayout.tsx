38d6236c8cf34cf5d9610b7dfa958ca46b3e3d68
Mohamed mahmoud <mohamedmkamal19@gmail.com>
2024-04-06 06:03:31 +0200

    file project

---
+++ src/Layouts/MainLayout/MainLayout.tsx
@@ -0,0 +1,13 @@
+import { Container } from "react-bootstrap"
+
+const MainLayout = () => {
+  return (
+    <Container className='g'>
+         <h1 className=" font-bold underline">
+      Hello world!
+    </h1>
+        MainLayout</Container>
+  )
+}
+
+export default MainLayout
---
+++ src/main.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client'
+import MainLayout from './Layouts/MainLayout/MainLayout'
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+import './index.css'
+ReactDOM.createRoot(document.getElementById('root')!).render(<MainLayout/>
+)
